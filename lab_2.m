504a76b332e5aaeb3d362bebc1b20d02c0932d83
BIGWOJ <wojtek.latos@interia.pl>
2025-03-17 16:05:37 +0100

    lab 2 done

---
+++ lab_2.m
@@ -0,0 +1,10 @@
+clc
+close all
+clear all
+
+% W simulinku zaczynamy od zmiany równania, żeby po lewej stronie była
+% największa tylko pochodna 
+
+% Warunki początkowe wpisujemy tam, gdzie wyjściem jest ta zmienna
+%np. x'(0) = 2, to ustawiamy tam gdzie jest wyjście 1sza pochodna i dajemy
+%2
