cf6a3450d6b1242584c326d5ac2873d36c3d40f3
BIGWOJ <wojtek.latos@interia.pl>
2025-03-31 21:32:02 +0200

    Lab 4 done

---
+++ lab_4/lab_4.m
@@ -0,0 +1,21 @@
+clc
+close all
+clear all
+
+out = sim("lab_4_simulink.slx");
+x = out.x_output.Data
+y = out.y_output.Data
+
+figure
+hold on
+xlim([-10,10]);
+ylim([-10,10]);
+rectangle('Position', [-5 -5 10 10], 'Curvature', [1 1]);
+plot(x, y, 'b');
+
+for i=1:length(x)
+    satelitte = plot(x(i), y(i), 'ko')
+    pause(0.01)
+    delete(satelitte)
+end
+
