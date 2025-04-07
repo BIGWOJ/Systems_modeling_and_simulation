b6092501a6292b6d09ef4c9318d52664fb6abd2b
BIGWOJ <wojtek.latos@interia.pl>
2025-04-07 21:38:37 +0200

    Lab 5 done

--- lab_4/lab_4.m
+++ lab_4/lab_4.m
@@ -3,8 +3,8 @@ close all
 clear all
 
 out = sim("lab_4_simulink.slx");
-x = out.x_output.Data
-y = out.y_output.Data
+x = out.x_output.Data;
+y = out.y_output.Data;
 
 figure
 hold on
@@ -14,7 +14,7 @@ rectangle('Position', [-5 -5 10 10], 'Curvature', [1 1]);
 plot(x, y, 'b');
 
 for i=1:length(x)
-    satelitte = plot(x(i), y(i), 'ko')
+    satelitte = plot(x(i), y(i), 'ko');
     pause(0.01)
     delete(satelitte)
 end
